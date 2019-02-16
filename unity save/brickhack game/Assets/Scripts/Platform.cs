d41d7711c4ff51e54fb85526f40eaecae5239b60
LAPTOP-PSDUIGH6\Nick <nlr3026@rit.edu>
2019-02-16 13:40:10 -0500

    started platforms

---
+++ unity save/brickhack game/Assets/Scripts/Platform.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public bool inPlacementMode;
+    public float speed = 0.1f;
+
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        inPlacementMode = false;
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            inPlacementMode = !inPlacementMode;
+        }
+
+        if (inPlacementMode)
+        {
+            MovePlatform();
+        }
+
+        transform.position = position;
+	}
+
+    void MovePlatform()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.W) || y > 0)
+        {
+            position.y += speed;
+        }
+
+        if (Input.GetKey(KeyCode.A) || x < 0)
+        {
+            position.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.S) || y < 0)
+        {
+            position.y -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D) || x > 0)
+        {
+            position.x += speed;
+        }
+    }
+}
