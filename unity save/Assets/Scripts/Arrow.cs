e092b5a9ebc222c8c1e9982784562eeb48edf561
dms7827 <dms7827@rit.edu>
2019-02-16 21:45:24 -0500

    trap art

---
+++ unity save/Assets/Scripts/Arrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Arrow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    private void Update()
+    {
+        transform.position += (Vector3)velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Arrow")) { return; }
+
+        if (collision.CompareTag("Player"))
+        {
+            //DO DAMAGE TO PLAYER HERE
+        }
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ unity save/Assets/Scripts/ArrowFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFire : MonoBehaviour {
+    [SerializeField] private GameObject ArrowPrefab;
+
+    private GameObject myArrow;
+
+    private void Awake()
+    {
+        myArrow = Instantiate(ArrowPrefab);
+        myArrow.SetActive(false);
+    }
+
+    //called by animator
+    public void FireArrow()
+    {
+        myArrow.SetActive(true);
+        myArrow.transform.position = transform.position;
+    }
+}
