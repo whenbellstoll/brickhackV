a5c5b34a2cb511bd657f2e4ff974a4ee9e1ba4d8
dms7827 <dms7827@rit.edu>
2019-02-16 23:42:10 -0500

    main menu

---
+++ unity save/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField] private string gameScene;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+        }
+	}
+}
