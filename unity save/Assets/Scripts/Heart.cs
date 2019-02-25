3a0104ae747e973a0305642be8796c774910768e
DESKTOP-8BHSMK1\kentr <kxr6031@rit.edu>
2019-02-25 17:38:16 -0500

    Added the healing heart

--- unity save/Assets/Scripts/Heart.cs
+++ unity save/Assets/Scripts/Heart.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Heart : MonoBehaviour {
+    [SerializeField] GameObject p1;
+    [SerializeField] GameObject p2;
+    [SerializeField] BoxCollider2D myBox;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		//Find reference
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+        if (p1.GetComponent<BoxCollider2D>().bounds.Intersects(myBox.bounds))
+        {
+            if(p1.GetComponent<Player>().currentHealth < p1.GetComponent<Player>().baseHealth)
+            {
+                p1.GetComponent<Player>().currentHealth++;
+                Destroy(gameObject);
+            }
+            
+        }
+
+        if (p2.GetComponent<BoxCollider2D>().bounds.Intersects(myBox.bounds))
+        {
+            if (p2.GetComponent<Player>().currentHealth < p2.GetComponent<Player>().baseHealth)
+            {
+                p2.GetComponent<Player>().currentHealth++;
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
 }
+
