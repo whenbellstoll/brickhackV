31b3d130687a2ee1df52ed1f895d42171cdb88cb
DESKTOP-8BHSMK1\kentr <kxr6031@rit.edu>
2019-02-25 15:36:53 -0500

    Added Heart script, not started

---
+++ unity save/Assets/Scripts/Heart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		//Find reference
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
