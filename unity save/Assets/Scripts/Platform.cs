a89eeaa056e85f671c56e22bd75de83961a7abc8
Nick <nickras35@gmail.com>
2019-03-06 05:27:43 -0500

    Tried to make the platforms move. Everything is bad. I'll fix it tomorrow.

--- unity save/Assets/Scripts/Platform.cs
+++ unity save/Assets/Scripts/Platform.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Platform : MonoBehaviour {
+    [SerializeField] private float range;
+    [SerializeField] private float velocity = 0.1f;
+    private float posInRange;
 
-    
+    Vector3 position;
+
 	// Use this for initialization
 	void Start ()
     {
-
+        posInRange = Random.Range(-range / 2, range / 2);
+        position = transform.position;
+        position.x += posInRange;
+        transform.position = position;
 	}
 
     // Update is called once per frame
     protected virtual void Update()
     {
+        if (posInRange > range / 2 || posInRange < -range / 2)
+        {
+            velocity *= -1;
+        }
 
+        position.x += velocity;
+        posInRange += velocity;
 
-        
+        transform.position = position;
     }
 
     public void Rotate()
