239ab2377e5270eff3e62226f07dc771e8ba45bb
DESKTOP-8BHSMK1\kentr <kxr6031@rit.edu>
2019-03-06 16:26:14 -0500

    G L A D I A T O R G U L L I S D E A D

--- unity save/Assets/Scripts/ArrowFire.cs
+++ unity save/Assets/Scripts/ArrowFire.cs
@@ -6,33 +6,19 @@ public class ArrowFire : MonoBehaviour {
     [SerializeField] private GameObject ArrowPrefab;
 
     private GameObject myArrow;
-    bool facingLeft;
+    short firingDirection; //direction that the arrow is fired based on one of four values.
+
 
     private void Awake()
     {
-        if(transform.position.x < 0)
-        {
-            facingLeft = true;
-        }
-        else
-        {
-            facingLeft = false;
-        }
+        firingDirection = 0;
         myArrow = Instantiate(ArrowPrefab);
         myArrow.SetActive(false);
     }
 
     private void Update()
     {
-        if (transform.position.x < 0)
-        {
-            facingLeft = true;
-        }
-        else
-        {
-            facingLeft = false;
-        }
-        GetComponent<SpriteRenderer>().flipX = !facingLeft;
+      
     }
 
     //called by animator
@@ -40,15 +26,44 @@ public class ArrowFire : MonoBehaviour {
     {
         PlatformManager.traps.Add(myArrow);
         myArrow.SetActive(true);
-        if (facingLeft)
+        Arrow myArrowArrowC = myArrow.GetComponent<Arrow>();
+        if (firingDirection == 0)
         {
-            myArrow.GetComponent<Arrow>().velocity.x = -10;
-        }
-        else
-        {
-            myArrow.GetComponent<Arrow>().velocity.x = 10;
+            myArrowArrowC.velocity.x = -10;
+            myArrowArrowC.velocity.y = 0;
+            myArrow.transform.right = new Vector2(1, 0);
 
+        }
+        else if( firingDirection == 1)
+        {
+            myArrowArrowC.velocity.x = 0;
+            myArrowArrowC.velocity.y = 10;
+            myArrow.transform.right = new Vector2(0, -1);
+
+        }
+        else if( firingDirection == 2)
+        {
+            myArrowArrowC.velocity.x = 10;
+            myArrowArrowC.velocity.y = 0;
+            myArrow.transform.right = new Vector2(-1,0);
+        }
+        else if( firingDirection == 3 )
+        {
+            myArrowArrowC.velocity.x = 0;
+            myArrowArrowC.velocity.y = -10;
+            myArrow.transform.right = new Vector2(0, 1);
         }
         myArrow.transform.position = transform.position;
     }
+
+    public void RotateArrow()
+    {
+        firingDirection++;
+
+        if(firingDirection > 3)
+        {
+            firingDirection = 0;
+        }
+    }
+
 }
